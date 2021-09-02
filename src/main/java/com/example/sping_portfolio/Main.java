7479998b108f9736de4ce83ec37717bfbc09dce8
varnit05 <dakidee05@gmail.com>
2021-09-02 08:58:05 -0700

    this is the finalavg code

--- src/main/java/com/example/sping_portfolio/Main.java
+++ src/main/java/com/example/sping_portfolio/Main.java
@@ -6,8 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
+            public static void main(String[] args){
+                int grade1;
+                grade1 = 1;
+                int grade2;
+                grade2 = 2;
+                int grade3;
+                grade3 = 3;
+                int grade4;
+                grade4 = 4;
+                int grade5;
+                grade5 = 5;
+                float avg12 = (grade1 + grade2) / 2;
+                float avg34 = (grade3 + grade4) / 2;
+                float finalAvg = (avg12 / 5 + avg34 / 5 + 3 * grade5/ 5) / 3;
+                System.out.println(avg12);
+                System.out.println(avg34);
+                System.out.println(finalAvg);
+            }
+        }
 
-}
+    
